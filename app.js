fdd77fbf3eec0f80a4fb6c57dc1a89f532055566
scomonty <scomonty@gmail.com>
2014-06-02 11:55:16 -0700

    part 5

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
-$(document).ready(function(){
-	$('.ryu').mouseenter(function(){
-  		$('.ryu-still').hide();
-  		$('.ryu-ready').show();
-})
-	.mouseleave(function(){
-		$('.ryu-ready').hide();
-		$('.ryu-still').show();
-	})
-	.mousedown(function(){
-	// play hadouken sound
-	$('.ryu-ready').hide();
-	$('.ryu-throwing').show();
-	$('.hadouken').show();
-	// animate hadouken to the right of the screen
-	})
-	.mouseup(function(){
-		$('.ryu-throwing').hide();
-		$('.ryu-ready').show();
-	});
+$(document).ready(function() {
+  $('.ryu').mouseenter(function() {
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+  })
+  .mouseleave(function() {
+    $('.ryu-ready').hide();
+    $('.ryu-still').show();
+  })
+  .mousedown(function() {
+    playHadouken();    
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken').finish().show()
+    .animate(
+      {'left': '1000px'},
+      500,
+      function() {
+        $(this).hide();
+        $(this).css('left', '500px');
+      }
+    );
+  })
+  .mouseup(function() {
+    $('.ryu-throwing').hide();
+    $('.ryu-ready').show();
+  });
 });
+  
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
